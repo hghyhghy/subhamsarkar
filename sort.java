2d9d09b561899e6f228639a3221f37f9c27c1bba
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-29 22:20:30 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class sort{
+    public static void main(String[]args){
+
+        int[] marks= new int [4];
+        marks[0]=88;
+        marks[1]=78;
+        marks[2]=89;
+        marks[3]=100;
+
+
+    Arrays.sort(marks);
+        System.out.println(marks[0]);
+
+    }
+}
