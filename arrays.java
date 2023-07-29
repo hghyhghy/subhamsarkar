159e90bac276e074178fc0a06d70a438941a8759
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-29 21:39:15 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,21 @@
+
+
+
+
+public class arrays{
+   
+   
+   
+    public static void main(String[] args){
+        
+        // arrays
+
+        int[] marks= new int[3];
+         marks[0]=67;
+         marks[1]=77;
+         marks[2]=87;
+
+         System.out.println(marks[0]);
+         
+    }
+}
