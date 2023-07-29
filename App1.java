5aaf2a7a084b36f8f125cb9454ded99e46579c06
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-29 21:13:05 +0530

    Add files via upload

---
+++ App1.java
@@ -0,0 +1,9 @@
+
+public class App1 {
+    public static void main(String[] args){
+        // charAt
+        //replace
+        String name1="subham and sayantani";
+        System.out.println(name1.substring(7, 11));
+    }
+}
