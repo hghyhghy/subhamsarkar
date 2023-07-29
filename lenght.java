336fc1229b2516785280c2eca34af30a0d3a92c0
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-29 22:06:14 +0530

    Add files via upload

---
+++ lenght.java
@@ -0,0 +1,18 @@
+
+
+
+public class lenght{
+    public static void main(String[] args){
+// lenght of arrays
+
+    int[] marks= new int[4];
+    marks[0]=87;
+    marks[1]=77;
+    marks[2]=97;
+System.out.println(marks.length);
+
+
+
+    
+}
+}
