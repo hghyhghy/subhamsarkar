185be3af6c14c6f585a08af3a51ac1ced705960c
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 23:32:10 +0530

    Add files via upload

---
+++ mainjava.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class mainjava{
+    
+    public static void main(String[] args){
+        //how ro take input in java
+        
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Input your age:");
+        int age=sc.nextInt();
+
+    
+
+        //System.out.println(age);
+
+        //int age =sc.nextInt();
+        int votingage=18;
+
+        if(age>=votingage)
+        {
+            System.out.println("APPLICABE FOR VOTING:");
+            
+         } else{
+            System.out.println("NOT APPLICABLE FOR VOTING:");
+        }
+
+
+
+    }
+}
