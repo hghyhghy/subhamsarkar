aeae7a3548223232f416aa69951208235e051705
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 22:34:30 +0530

    Add files via upload

---
+++ modulo.java
@@ -0,0 +1,34 @@
+
+public class modulo{
+    public static void main(String[] args){
+        
+        //modulus
+
+        double a=5;
+        
+        double b= 4;
+
+        double modulus=a%b;
+
+        System.out.println(modulus);
+
+        //operator
+// increment
+//decrement
+//unary operator
+        int number=4;
+
+        number++;
+
+         System.out.println(number);
+
+         int number=7;
+          
+         number --;
+
+         System.out.println(number);
+
+
+
+    }
+}
