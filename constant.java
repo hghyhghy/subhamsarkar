b19d04a959fcdb8f5ca2f23780c1161fc31c279e
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 22:16:51 +0530

    Add files via upload

---
+++ constant.java
@@ -0,0 +1,15 @@
+
+public class constant{
+    public static void main(String[] args){
+
+        // constant
+
+        double a=34;
+
+        double b=39;
+        
+        double div=a/b;
+         System.out.println(div);
+
+    }
+}
