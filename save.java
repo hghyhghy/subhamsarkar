3e26b01937abb26d27909153603051e2b5b61f14
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 22:01:05 +0530

    Add files via upload

---
+++ save.java
@@ -0,0 +1,24 @@
+
+public class save{
+
+    public static void main(String[]args){
+        
+
+       double price=100.00;
+       double finalprice= price + 18 ;
+       System.out.println(finalprice);
+
+
+
+    }
+}
+
+public class save{
+    public static void main(String[] args){
+
+        int p=100;
+        int tp=p+(int) 18.00;
+        System.out.println(tp);
+        
+    }
+}
