2b3d2b23336c4d6f2e2f41178eaa7125bfd7f9d6
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 22:48:16 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,15 @@
+
+public class operator{
+    
+    public static void main(String[] args){
+        // maths.max
+
+   
+        //System.out.println(Math.max(56,89));
+
+        // maths.min
+
+        System.out.println(Math.min(76, 57));
+
+    }
+}
