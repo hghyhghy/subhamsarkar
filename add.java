c410ed9a84ab78bf0d225d5d12f97acd4909f46e
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 07:58:34 +0530

    Update add.java

--- add.java
+++ add.java
@@ -12,3 +12,15 @@ public class add{
     System.out.println(substraction);
 }
 }
+
+public class add{
+    public static void main(String[]args){
+
+    int first=45;
+    int second=34;
+
+    int substraction=first-second;
+
+    System.out.println(substraction);
+}
+}
