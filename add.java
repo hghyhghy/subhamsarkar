f695affee014d4191c4abf9c263aa58d4957a90d
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-30 07:56:58 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+
+
+
+public class add{
+    public static void main(String[]args){
+
+    int first=45;
+    int second=34;
+
+    int substraction=first-second;
+
+    System.out.println(substraction);
+}
+}
