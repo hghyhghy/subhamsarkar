0b624f73c188c22eef134653ee1a75e9020d00d8
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-07-31 08:09:55 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,18 @@
+
+public class logical{
+
+    public static void main(String[]args){
+        
+        //logical operator
+
+        int a=18;
+        int b=70;
+
+        if(a>18&&b<80)
+
+        {
+            System.out.println("HE IS ADULT:");
+        }
+        
+    }
+}
