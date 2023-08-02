266e5ba682dc2c030f55929b6902ceef814a0e60
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-02 21:28:28 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+
+public class pattern3
+{
+  public static void main(String[]args)
+  {
+
+    int n=10;
+
+    for(int i=1;i<=7;i++)
+    {
+
+        for (int j=1;j<=i;j++)
+        {
+            System.out.print(j);
+
+        }
+        System.out.println();
+    }
+  }  
+}
