e56ad8eaca21334e3d6ed995253971faed4cd1eb
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-02 11:05:46 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,23 @@
+
+public class function
+{
+    public static void main(String[]args)
+    {
+        int i=0;
+         while (true)
+         {
+if(i==3)
+{
+    i=i+1;
+    continue;
+}
+         save.out.println(i);
+        i=i+1;
+        if(i>=5)
+        {
+            break;
+        }
+          }  
+    }
+
+}
