03907387344715b621ff2fd4192767b4fa9d2e5f
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-02 12:20:00 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,22 @@
+
+public class methods{
+     public static void printsum(int a, int b){
+
+        int sum= a+b;
+        
+       System.out.println(sum);
+
+      }
+
+        public static void printName(String name){
+            System.out.println(name);
+        }
+         public static void main(String[]args){
+
+            
+            printsum(4, 6);
+
+            printName("SUBHAM THE BAD ASS");
+         }
+    }
+
