6eadd8f3c5a9216d0b7c45a249a35d6b52ecd3f0
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-06 16:43:38 +0530

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,36 @@
+
+public class Arrays1
+{
+
+    public static void main(String[]args)
+    {
+
+        int [] marks=new int[3];
+
+        marks[0]=100;
+        marks[1]=98;
+        marks[2]=99;
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+        if( marks[0]>marks[1] && marks[0]>marks[2])
+        {
+            int highest=marks[0];
+
+            System.out.print(highest);
+
+
+
+
+
+
+        }
+        System.out.println("  is the highest obtained marks:");
+
+        
+
+        
+}
+}
