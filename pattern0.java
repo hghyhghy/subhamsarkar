d5c11c2b182c36443922ce3ac9617203916653db
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-13 09:25:16 +0530

    Add files via upload

---
+++ pattern0.java
@@ -0,0 +1,32 @@
+//import java.util.Scanner;
+
+public class pattern0 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb1 = new StringBuilder("S");
+
+        System.out.println(sb1);
+
+        sb1.append("U");
+
+        System.out.println(sb1);
+
+        sb1.append("B");
+
+        System.out.println(sb1);
+
+        sb1.append("H");
+
+        System.out.println(sb1);
+
+        sb1.append("A");
+
+        System.out.println(sb1);
+
+        sb1.append("M");
+
+        System.out.println(sb1);
+
+    }
+}
