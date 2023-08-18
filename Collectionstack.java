64bc38ab0beebb17e170a6c40773009dd482d476
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-08-18 12:09:57 +0530

    Add files via upload

---
+++ Collectionstack.java
@@ -0,0 +1,85 @@
+import java.util.*;
+public class Collectionstack
+{
+
+
+  public static void main(String[]args)
+  {
+      // creating an object of class stack
+
+    Stack<Integer> st = new Stack<Integer> ();
+
+    // checking if the stack is empty or not
+
+    boolean result= st.empty();
+
+    //printing the statement
+
+    System.out.println("is the stack empty" + " " + result);
+
+
+    //pushing element in the stack
+
+
+    st.push(23);
+
+    st.push(50);
+
+    st.push(100);
+
+    st.push(2000);
+
+    st.push(20);
+
+
+    
+
+
+
+    
+
+
+    System.out.println("the elements are "+ st);
+
+    result= st.empty();
+
+    System.out.println("is the stack is empty" + " " + result);
+
+
+    st.pop();
+
+    st.pop();
+
+    System.out.println("the result is "  + " " + st);
+
+  //peek operation to get the head of the stack 
+     
+
+    System.out.println("the element at the top of the stack is " + " " + st.peek());
+
+    System.out.println(st);
+
+  
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+    
+
+
+  }
+
+
+}
